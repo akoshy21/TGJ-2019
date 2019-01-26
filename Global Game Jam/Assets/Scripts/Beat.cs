bcc8431ab7ea35110ea878ea0596b70e8a42de0f
Unknown <ak6429@nyu.edu>
2019-01-26 13:32:15 -0500

    UI Dialogue Shit

---
+++ Global Game Jam/Assets/Scripts/Beat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    public string prompt;
+
+    public string rectResponse;
+    public bool rectReact = true; // if true, rectangle has a 'Reaction' and does not trigger 'rectPrompt' 
+    public Beat rectPrompt;
+
+    public string circResponse;
+    public bool circReact = true;
+    public Beat circPrompt;
+
+    public string triResponse;
+    public bool triReact = true;
+    public Beat triPrompt;
+
+    public string xResponse;
+    public bool xReact = true;
+    public Beat xPrompt;
+}
