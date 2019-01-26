3e7af763a7435a18486ba312c238033cc9c3765c
Unknown <rmh472@nyu.edu>
2019-01-25 21:31:05 -0500

    Scene for 2D game

---
+++ Global Game Jam/Assets/Player_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Rigidbody2D RB;
+    public BoxCollider2D BC;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
