fb9973795c2c8be701798817cf1f8aa0661c5f06
Unknown <rmh472@nyu.edu>
2019-01-25 23:05:55 -0500

    The boy moves and we've got borders on that camera

--- Global Game Jam/Assets/Player_Controller.cs
+++ Global Game Jam/Assets/Player_Controller.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    public Rigidbody2D RB;
-    public BoxCollider2D BC;
+    public Rigidbody2D rb;
+    public BoxCollider2D bc;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    public float pSpd;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (Input.GetKeyDown(upKey))
+        {
+            rb.velocity = new Vector2(0, 1);
+        }
+        if (Input.GetKeyDown(downKey))
+        {
+            rb.velocity = new Vector2(0, -1);
+        }*/
+
+        rb.velocity = new Vector2(Input.GetAxis("LHorizontal")*pSpd, Input.GetAxis("LVertical")*pSpd);
+
+      //  Debug.Log(rb.velocity);
+      //  Debug.Log("Vertical: " + Input.GetAxis("LVertical"));
+      //  Debug.Log("Horizontal: " + Input.GetAxis("LHorizontal"));
     }
 }
